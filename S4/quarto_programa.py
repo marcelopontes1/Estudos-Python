30eb441db603186ccf78f7f7b5ed6aa24965c608
Marcelo Henrique PONTES DE SOUSA <marcelopontes.tele@gmail.com>
2021-07-27 07:49:23 -0300

    Atualizando programas

--- S4/quarto_programa.py
+++ S4/quarto_programa.py
@@ -1,5 +1,7 @@
-num_real = 4.55
+num = input('Digite um número para calcular o quadrado dele: ')
+
+num_real = float(num)
 
 quadrado_num_real = num_real ** 2
 
-print(quadrado_num_real)
+print(f'O valor do quadrado desse número é: {quadrado_num_real}')
