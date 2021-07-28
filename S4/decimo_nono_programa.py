c0145b7b77deee7e290d8d95e8ae40c82d326ed9
Marcelo Henrique PONTES DE SOUSA <marcelopontes.tele@gmail.com>
2021-07-28 08:38:30 -0300

    Adicionando segunda página de exercícios

---
+++ S4/decimo_nono_programa.py
@@ -0,0 +1,7 @@
+vol_litros = input('Digite o valor de volume em litros: ')
+
+vol_litros_real = float(vol_litros)
+
+vol_metros_cubicos = vol_litros_real/1000
+
+print(f'O volume em metros cúbicos é de {vol_metros_cubicos}')
---
+++ S4/decimo_setimo_programa.py
@@ -0,0 +1,7 @@
+comp_cm = input('Digite o comprimento em centímetros: ')
+
+comp_cm_real = float(comp_cm)
+
+comp_pol = comp_cm_real/2.54
+
+print(f'O comprimento em polegadas é de {comp_pol}')
