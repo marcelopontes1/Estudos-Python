bc38e20eaaffc327eead523d31bb99a48c5df610
Marcelo Henrique PONTES DE SOUSA <marcelopontes.tele@gmail.com>
2021-07-29 11:50:05 -0300

    Corrigindo texto no exercício de número 39

--- S4/trigesimo_nono_programa.py
+++ S4/trigesimo_nono_programa.py
@@ -5,5 +5,5 @@ segundo_premio = valor_total * 0.32
 terceiro_premio = valor_total - primeiro_premio - segundo_premio
 
 print(f'O primeiro ganhador receberá o valor de {primeiro_premio} reais')
-print(f'O primeiro ganhador receberá o valor de {segundo_premio} reais')
-print(f'O primeiro ganhador receberá o valor de {terceiro_premio} reais')
+print(f'O segundo ganhador receberá o valor de {segundo_premio} reais')
+print(f'O terceiro ganhador receberá o valor de {terceiro_premio} reais')
