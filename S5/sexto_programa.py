bc33f2014d4fea16893fac96f33a29e0d6f8564d
Marcelo Henrique PONTES DE SOUSA <marcelopontes.tele@gmail.com>
2021-08-04 12:19:58 -0300

    Adicionando primeira página de exercícios

---
+++ S5/sexto_programa.py
@@ -0,0 +1,11 @@
+num1 = input('Insira o primeiro número: ')
+num2 = input('Insira o segundo número: ')
+
+num1_int = int(num1)
+num2_int = int(num2)
+
+if num1_int > num2_int:
+    print(f'O número {num1_int} é maior do que {num2_int} e a diferença entre eles é de {num1_int - num2_int}')
+else:
+    print(f'O número {num2_int} é maior do que {num1_int} e a diferença entre eles é de {num2_int - num1_int}')
+    
