0b27e46f1c01afc30688719bafc2b50660e9663b
Marcelo Henrique PONTES DE SOUSA <marcelopontes.tele@gmail.com>
2021-08-16 13:58:08 -0300

    Editando arquivo do progama 17

--- S5/decimo_setimo_programa.py
+++ S5/decimo_setimo_programa.py
@@ -10,6 +10,8 @@ base_menor_convert = float(base_menor)
 base_maior_convert = float(base_maior)
 altura_convert = float(altura)
 
-area_total = area_trapezio(base_maior_convert, base_menor_convert, altura_convert)
-
-print(f'O valor da área do trapézio é de {area_total}')
+if base_menor_convert > 0 and base_maior_convert > 0:
+    area_total = area_trapezio(base_maior_convert, base_menor_convert, altura_convert)
+    print(f'O valor da área do trapézio é de {area_total}')
+else:
+    print('Valores inválidos')
