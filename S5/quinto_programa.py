ced0ba26a1d9bc02f0ce8417e8ff2062527f2ca6
marcelopontes1 <41488586+marcelopontes1@users.noreply.github.com>
2021-09-10 07:11:50 -0300

    Removendo programas

--- S5/quinto_programa.py
+++
@@ -1,9 +0,0 @@
-num = input('Insira um número: ')
-
-num_int = int(num)
-
-if num_int % 2 == 0:
-    print(f'O número {num_int} é par')
-else:
-    print(f'O número {num_int} é ímpar')
-    
--- S5/segundo_programa.py
+++
@@ -1,9 +0,0 @@
-num = input('Insira um número inteiro: ')
-
-num_int = int(num)
-
-if num_int >= 0:
-    raiz = num_int ** 0.5
-    print(f'A raiz quadrada do número é {raiz}')
-else:
-    print('O número é inválido')
