a8a8a8ffb47a7d6c46ebad35f391fb52709de6b1
Marcelo Henrique PONTES DE SOUSA <marcelopontes.tele@gmail.com>
2021-09-10 06:37:08 -0300

    Apagando programas

--- S4/primeiro_programa.py
+++
@@ -1,5 +0,0 @@
-num = input('Digite o número inteiro: ')
-
-num_int = int(num)
-
-print(f'O número inteiro é: {num_int}')
--- S4/segundo_programa.py
+++
@@ -1,5 +0,0 @@
-num = input('Digite um número real: ')
-
-num_real = float(num)
-
-print(f'O número real é: {num_real}')
